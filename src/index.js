6fdde321d4aca4640fb42a019bc708c0d25e0f18
Vitali Herasimenka <vitttold@inbox.ru>
2021-03-12 16:13:26 +0300

    add: solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+
+  let str = String(Math.abs(n));
+  let res = "";
+  for (let i = 0; i < str.length; i++ ) {
+    res = str[i] + res
+  }
+  return Number(res);
+
 }
